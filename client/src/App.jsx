7f4b3cad1e9a5ea9e937a2048b012397cd921abd
Surakit <surakitkunkayun@gmail.com>
2024-06-14 13:38:21 +0700

    feat: finished assignment

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,10 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
-
+import React from "react";
 import HomePage from "./pages/HomePage.jsx";
 import ViewProductPage from "./pages/ViewProductPage.jsx";
+
+export const UserDataContext = React.createContext();
 
 function App() {
   const userData = {
@@ -13,12 +15,15 @@ function App() {
 
   return (
     <div className="App">
+      <UserDataContext.Provider value={{user: userData}}
+      >
       <Router>
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/product/view/:id" element={<ViewProductPage />} />
         </Routes>
       </Router>
+      </UserDataContext.Provider>
     </div>
   );
 }
